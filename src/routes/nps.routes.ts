57c0cba81e24e54021446a3d848098416a9d2c0a
Paulo Freitas <42820569+paulofreitas-py@users.noreply.github.com>
2021-03-07 15:01:36 -0300

    Delete src directory

--- src/routes/nps.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { NpsController } from '../controllers/NpsController';
-import { SendMailController } from '../controllers/SendMailController';
-import { SurveysController } from '../controllers/SurveysController';
-
-const routes = Router();
-const controller = new NpsController();
-
-export default routes.get('/:survey', controller.execute);
